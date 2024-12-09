b11b0b180523ddfe66665ba6db6444dc603b0516
ele17 <131241638+ele17@users.noreply.github.com>
2024-12-09 14:23:22 +0100

    Update AVL.h

--- projet_cwire/codeC/AVL.h
+++ projet_cwire/codeC/AVL.h
@@ -14,3 +14,23 @@ int hauteur;
 struct AVLNoeud *fg;
 struct AVLNoeud *fd;
 }AVLNoeud;
+
+AVLNoeud* creerNoeud(int elmt, int capacité) {
+    AVLNoeud *noeud = malloc(sizeof(AVLNoeud));
+    noeud->id = id;
+    noeud->capacité = capacité;
+    noeud->consommation = 0;
+    noeud->hauteur = 1;
+    noeud->gauche = noeud->right = NULL;
+    return noeud;
+}
+
+void freeAVL(AVLNode *racine) {
+    if (racine) {
+        freeAVL(racine->gauche);
+        freeAVL(racine->droit);
+        free(racine);
+    }
+}
+
+
