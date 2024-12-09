ca24f76cc69f65f27734ce07dca56bf04a58fee0
ele17 <131241638+ele17@users.noreply.github.com>
2024-12-09 13:59:00 +0100

    Update AVL.h

--- projet_cwire/codeC/AVL.h
+++ projet_cwire/codeC/AVL.h
@@ -1,1 +1,16 @@
+#define AVL_H
+#define AVL_H 1000
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct AVLNode{
+int id;
+int capacity;
+int consumption;
+int height;
+struct AVLNode *fg;
+struct AVLNode *fd;
+}AVLNode;
