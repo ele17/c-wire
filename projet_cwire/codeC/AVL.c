94c3276020dd0357ce9050b3877b06d0b1edcf57
ele17 <131241638+ele17@users.noreply.github.com>
2024-12-09 16:06:20 +0100

    Update AVL.c

--- projet_cwire/codeC/AVL.c
+++ projet_cwire/codeC/AVL.c
@@ -1,2 +1,18 @@
 #include "avl.h"
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <fichier_csv>\n", argv[0]);
+        return 1;
+    }
+
+    Noeud *racine = processCSV(argv[1]);
+    if (racine == NULL) {
+        return 1;
+    }
+
+    printf("Données traitées:\n");
+    infixe(racine);
+  
+    return 0;
+}
