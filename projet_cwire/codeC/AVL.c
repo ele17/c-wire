f35833d37a9af30f88f49f56cd732a24cc4e98bc
ele17 <131241638+ele17@users.noreply.github.com>
2024-12-09 13:56:44 +0100

    Update AVL.c

--- projet_cwire/codeC/AVL.c
+++ projet_cwire/codeC/AVL.c
@@ -1,46 +1,20 @@
+#define AVL_H
+#define AVL_H 1000
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct AVLNode{
+int id;
+int capacity;
+int consumption;
+int height;
+struct AVLNode *fg;
+struct AVLNode *fd;
+}AVLNode;
 
 
-typedef struct StateAVL {
-double d;
-double d_min;
-double d_max;
-double d_max_min;
-double d_moy;
-int h;
-int nb_state;
-struct StateAVL *fg;
-struct StateAVL *fd;
-} StateAVL;
-
-typedef struct Road {
- 	StateAVL* node;
- 	struct Road* next;
-    struct Road *end;
-} Road;
-
-int Max(int a, int b);
-int height(StateAVL *node);
-int Balanced(StateAVL* root);
-
-StateAVL* rotateRight(StateAVL* b);
-StateAVL *rotateLeft(StateAVL *a);
-StateAVL* createNode(int Route_id, double d);
-Road* changeRoad(Road* root, StateAVL* newState);
-Road *insertPliste(Road *pliste, StateAVL *newState);
-StateAVL *insertAVL(StateAVL *root, StateAVL *newState);
-StateAVL *searchSmoller(StateAVL *root);
-StateAVL *heightBalanced(StateAVL *root);
-StateAVL *suppSmoler(StateAVL *root);
-StateAVL *insertAVLFromList(Road *pliste, StateAVL *a);
-
-
-void SortedData(struct StateAVL* newNode, struct StateAVL* sortedData[ ], int* Index);
-void freeSortedData(struct StateAVL* sortedData[ ], int Index);
-void CreateCSV(struct StateAVL* root);
 
 
